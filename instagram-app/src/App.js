a96982d282fa5eeb2d8787909228b20efe852f2d
Jason Belgard <belgard.jason@gmail.com>
2019-06-05 15:47:51 -0500

    Styling Login

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import withAuthenticate from './components/Authentication/withAuthenticate';
 import PostsPage from './components/PostContainer/PostsPage';
+import Login from './components/Login/Login'
 
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage, Login);
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <PostsPage />
+        <ComponentFromWithAuthenticate />
       </div>
     );
   }
---
+++ instagram-app/src/components/Authentication/withAuthenticate.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+const withAuthenticate = (PostPage, Login) => props => {
+    if (localStorage.getItem('username')) {
+        return <PostPage {...props} />;
+    }
+    return <Login {...props} />;
+};
+
+export default withAuthenticate
