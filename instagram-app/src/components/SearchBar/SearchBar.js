462a845ccc0ca6409429ccc800d27deb2e430204
Jason Belgard <belgard.jason@gmail.com>
2019-06-04 22:43:00 -0500

    added data in comments and post-day 1

---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+function SearchBar() {
+    return (
+        <div>
+            <input type='text' />
+        </div>
+    );
+}
+
+export default SearchBar
